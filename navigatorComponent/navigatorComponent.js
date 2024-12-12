3edff01a14ee9b396174659b4b1980cf000b4aef
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-12-12 19:32:47 +0100

    ultime modifiche

--- navigatorComponent/navigatorComponent.js
+++ navigatorComponent/navigatorComponent.js
@@ -10,14 +10,14 @@ export const createNavigator = (parentElement) => {
    const pages = Array.from(parentElement.querySelectorAll(".page"));
    
    const render = () => {
-      
       const url = new URL(document.location.href);
       const pageName = url.hash.replace("#", "");
-      const selected = pages.filter((page) => page.id === pageName)[0] || pages[0];
+      const selectedPage = pages.filter((page) => page.id === pageName)[0] || pages[0];
 
       pages.forEach(p => hide(p));
-      show(selected);
+      show(selectedPage);
    }
+   
    window.addEventListener('popstate', render); 
    render();   
  }
